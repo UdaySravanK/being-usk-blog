91a985bb02e1b97eafc7320b20f28a5b5fa7a4c9
UdaySravanK <UdaySravanKumar.Kamineni@target.com>
2019-04-28 15:40:27 -0500

    Updates

---
+++ precache-manifest.1abe9302073d9ed9c0a70d1dd31afa08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceadb9fb3de8014fce07df91bb4c15b6",
+    "url": "/being-usk-blog/index.html"
+  },
+  {
+    "revision": "518daf387cb0fe8db5e9",
+    "url": "/being-usk-blog/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/being-usk-blog/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "518daf387cb0fe8db5e9",
+    "url": "/being-usk-blog/static/js/main.aa6a22db.chunk.js"
+  },
+  {
+    "revision": "40f2bd85eac6645cf5b5",
+    "url": "/being-usk-blog/static/js/runtime~main.2f40318f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/being-usk-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
